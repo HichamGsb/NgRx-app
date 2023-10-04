c7256c4db12828e3bb1c17d52f832bd062f2b7d4
Hicham Guessab <hicham.guessab@cbainfo.fr>
2023-10-04 11:18:05 +0200

    An Alternative Way Of Defining Actions

--- src/app/store/counter.actions.ts
+++ src/app/store/counter.actions.ts
@@ -1,6 +1,18 @@
-import { createAction, props } from "@ngrx/store";
+// import { Action, createAction, props } from "@ngrx/store";
 
-export const increment = createAction(
-  '[Counter] Increment',
-  props<{value: number}>() // The shape of the data attached to the action
-);
+// export const increment = createAction(
+//   '[Counter] Increment',
+//   props<{value: number}>() // The shape of the data attached to the action
+// );
+
+import { Action } from "@ngrx/store";
+
+export const INCREMENT = '[Counter] Increment'
+
+export class IncrementAction implements Action {
+  readonly type = INCREMENT;
+
+  constructor(public value: number) {}
+}
+
+export type CounterActions = IncrementAction;
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,3 +1,6 @@
+import { CounterActions, INCREMENT, IncrementAction } from "./counter.actions";
+import { Action } from "@ngrx/store";
+
 const initialState = 0;
 
 // export const counterReducer = createReducer(
@@ -5,9 +8,9 @@ const initialState = 0;
 //   on(increment, (state, action) => state + action.value)
 // );
 
-export function counterReducer(state = initialState, action: any) {
-  if (action.type === '[Counter] Increment') {
-    return state + action.value;
+export function counterReducer(state = initialState, action: CounterActions | Action) {
+  if (action.type === INCREMENT) {
+    return state + (action as IncrementAction).value;
   }
   return state;
 }
